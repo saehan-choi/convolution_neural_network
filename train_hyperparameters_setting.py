ebb5f5a94ec30d4e6f0c036d2a66b5cabef7c027
saehan-choi <gmlssns5859@naver.com>
2022-01-08 22:19:09 +0900

    test

--- train_hyperparameters_setting.py
+++ train_hyperparameters_setting.py
@@ -16,52 +16,48 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 # parameters 말고 parameters()임.
 criterion = nn.CrossEntropyLoss().cuda()
 
-train_data = ImageFolder("./train", 
-                            transform=transforms.Compose
-                            ([transforms.Resize(256),
-                            transforms.RandomCrop(224),
-                            transforms.ToTensor(),
-                            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-                            ])
-                        )
-train_loader = DataLoader(train_data, batch_size=50, shuffle=True)
-wandb.init(project="my-test-project", entity="saehoni", name='haha')
 
-wandb.config = {
-  "learning_rate": 0.001,
-  "epochs": 10,
-  "batch_size": 50
-}
 
-# print(f'config_learning_rate:{wandb.config.learning_rate}')
-for epoch in range(5):
-    train_loss = 0.0
-    train_correct = 0
-    
-    for i, data in enumerate(train_loader, 0):
-        # get the inputs
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
-        # zero the parameter gradients
-        outputs = model(inputs.to("cuda"))
-        # _, predicted = torch.max(outputs, 1)
-        # print(labels)
-        # print(outputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item()
-        # train_correct += (predicted == labels).cpu().sum()
-    print(f'train_loss : {train_loss}')
-    print(f'epoch step :{epoch}')
-    wandb.log({"train_loss": train_loss})
-    wandb.watch(model)
+learning_rate = [0.001, 0.005, 0.0001]
+batch_size = [30, 40, 50]
 
-    # y = model(torch.randn(1, 3, 224, 224)).to("cuda")
-    # # print(y[0][1])
-    # print(y)
-    # print(y.size())
-wandb.log( {"learning_rate": 0.005,
-  "epochs": 50,
-  "batch_size": 5})
-# 이렇게하면 이 지점만 log 로 남음
+for lr in learning_rate:
+  for b_size in batch_size:
+    hyper_parameters = {
+        'learning_rate' : lr,
+        'epochs' : 10,
+        'batch_size' : b_size,
+        'storage_location' : 'C:\m_results\holy.txt'
+    }
+    # 이거 조심해야하는게, 실험시마다 text name 을 변경해줘야함 항상 넣어주지 않으면 겹쳐져서 헷갈림,......
+
+    f = open(hyper_parameters['storage_location'], 'a')
+    # 기존파일에 내용 추가하고싶을경우 'w' 가아닌 'a'로 해야함
+
+    train_data = ImageFolder("./train", 
+                                transform=transforms.Compose
+                                ([transforms.Resize(256),
+                                transforms.RandomCrop(224),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+                                ])
+                            )
+    train_loader = DataLoader(train_data, batch_size=hyper_parameters['batch_size'], shuffle=True)
+
+    for epoch in range(hyper_parameters['epochs']):
+        train_loss = 0.0
+        train_correct = 0
+        
+        for i, data in enumerate(train_loader, 0):
+            # get the inputs
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            # zero the parameter gradients
+            outputs = model(inputs.to("cuda"))
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+            # train_correct += (predicted == labels).cpu().sum()
+        print(f'train_loss {train_loss} epoch {epoch} learning_rate {lr} batch_size {b_size}')
+        f.write(f'train_loss {train_loss} epoch {epoch} learning_rate {lr} batch_size {b_size}\n')
--- train_hyperparameters_test.py
+++ train_hyperparameters_test.py
@@ -1,31 +1,63 @@
 import matplotlib.pyplot as plt
+from torch._C import LockingLogger
+from wandb.proto.wandb_telemetry_pb2 import Labels
 
-# x = [1,2,3,4,5,6,7,8,9,10,11]
-y0 = [2,3,4,5,5,1,2,7,1,1,0]
-y1 = [2,7,1,3]
-y2 = [2,7,1,5,1,2,6,1,3,8,4]
-y3 = [2,7,1,5,2,2,6,1,3,8,4]
-y4 = [2,7,1,5,6,2,6,1,3,8,4]
-y5 = [2,7,1,2,7,1,3,16,3,8,4]
-y6 = [2,7,1,5,1,2,2,7,1,3,16]
-y7 = [2,7,8,9,10,11,6,1,3,8,4]
-y8 = [2,7,1,5,7,8,9,10,11,8,4]
 
+
+
+
+f = open('C:\m_results\holy.txt','r')
+lines = f.readlines()
+num = 0
+
+
+# ★★★★★★★★★★★★★★★밑에 9를 자동적으로 받아올수 있게끔 해야함★★★★★★★★★★★★★★★
+# 이렇게 하면 많이 성공한거
 for i in range(9):
-    plt.plot(locals()[f'y{i}'],label = f'test{i+1}')
-    # 문자열로 된 변수명을 다시 지역
-    # 변수명으로 전환
-    # 전역변수로 하고싶다면 locals -> globals로 변환
-    # 어차피 x축이 epochs일거면 x축의 설정은 필요없음
-    # x축을 설정해버리면 x,y가 1대1대응이 되어야하므로 오히려 해가됨
+    locals()[f'loss_{i}'] = []
+    locals()[f'lr_{i}'] = []
+    locals()[f'batch_size_{i}'] = []
 
-plt.xlabel('epochs')
-plt.ylabel('loss')
 
-plt.legend()
-plt.show()
+for line in lines:
+    line = line.split()
+    print(line)
+    locals()[f'loss_{num}'].append(float(line[1]))
+    if line[3] == '9':
+        locals()[f'lr_{num}'].append(line[5])
+        locals()[f'batch_size_{num}'].append(line[-1])
+        num+=1
+        
+# print(lr_0)
+
+    # split하고나면 배열형태가 되기때문에 배열을 잡아줘야 split 다시 할 수 있음
+
+
+def plt_setting(amount_of_loss):
+    # f'y{i}'이거 loss_0, loss_1, loss_2 이런식으로 바꿔야함 들어올때 ㅎㅎ
+    for i in range(amount_of_loss):
+        # lr = lr_0
+        lr = globals()[f'lr_{i}']
+        bt_size = globals()[f'batch_size_{i}']
+        plt.plot(globals()[f'loss_{i}'],label = f'lr:{float(lr[0])},b_size:{int(bt_size[0])}')
+
+        # 문자열로 된 변수명을 다시 지역
+        # 변수명으로 전환
+        # ★★★★★ 전역변수로 하고싶다면 locals -> globals로 변환 ★★★★★
+        # 어차피 x축이 epochs일거면 x축의 설정은 필요없음
+        # x축을 설정해버리면 x,y가 1대1대응이 되어야하므로 오히려 해가됨
+
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+
+    plt.legend()
+    plt.show()
 
 
 # 근데 LOSS가 빨리떨어지는것이 꼭 좋은 HYPERPARAMERTERS라고 할수는 없는게, 
 # VALIDATION SET 에 OVERFITTING 되는것일수도 있기때문.
 # 다만 validation 에 최적화하는것은 무조건 가능
+
+
+plt_setting(8)
+f.close()
