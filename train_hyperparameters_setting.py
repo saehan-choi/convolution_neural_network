096a166cfb6fafac1b03a8a99084472322576852
saehan-choi <gmlssns5859@naver.com>
2022-01-09 11:10:48 +0900

    test

--- train_hyperparameters_setting.py
+++ train_hyperparameters_setting.py
@@ -1,37 +1,31 @@
 import torch
 import torch.nn
-import wandb
 from resnet import *
 import torch.optim as optim
 from torch.utils.data.dataloader import DataLoader
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
+import time
 
-
-device = torch.device('cuda')
-model = ResNet18(img_channel=3, num_classes=2)
-model = model.to(device)
-
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-# parameters 말고 parameters()임.
-criterion = nn.CrossEntropyLoss().cuda()
+# C:\Users\gmlss\OneDrive\바탕 화면\m_resnet\  example.txt 같이 저장하기
 
 
 
-learning_rate = [0.001, 0.005, 0.0001]
-batch_size = [30, 40, 50]
+
+learning_rate = [0.0001, 0.0005, 0.001, 0.005]
+batch_size = [40, 50, 60]
 
 for lr in learning_rate:
   for b_size in batch_size:
     hyper_parameters = {
         'learning_rate' : lr,
-        'epochs' : 10,
+        'epochs' : 20,
         'batch_size' : b_size,
-        'storage_location' : 'C:\m_results\holy.txt'
+        'storage_location' : 'C:\m_results\wow.txt'
     }
     # 이거 조심해야하는게, 실험시마다 text name 을 변경해줘야함 항상 넣어주지 않으면 겹쳐져서 헷갈림,......
 
-    f = open(hyper_parameters['storage_location'], 'a')
+    # f = open(hyper_parameters['storage_location'], 'a') 원래 여기있는데 잠시옮겨봄 실험해보자
     # 기존파일에 내용 추가하고싶을경우 'w' 가아닌 'a'로 해야함
 
     train_data = ImageFolder("./train", 
@@ -43,11 +37,20 @@ for lr in learning_rate:
                                 ])
                             )
     train_loader = DataLoader(train_data, batch_size=hyper_parameters['batch_size'], shuffle=True)
+    
+    device = torch.device('cuda')
+    model = ResNet18(img_channel=3, num_classes=2)
+    model = model.to(device)
+
+    # parameters 말고 parameters()임.
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    f = open(hyper_parameters['storage_location'], 'a')
 
     for epoch in range(hyper_parameters['epochs']):
         train_loss = 0.0
         train_correct = 0
-        
         for i, data in enumerate(train_loader, 0):
             # get the inputs
             inputs, labels = data
@@ -61,3 +64,4 @@ for lr in learning_rate:
             # train_correct += (predicted == labels).cpu().sum()
         print(f'train_loss {train_loss} epoch {epoch} learning_rate {lr} batch_size {b_size}')
         f.write(f'train_loss {train_loss} epoch {epoch} learning_rate {lr} batch_size {b_size}\n')
+        # f.close()
--- train_hyperparameters_test.py
+++ train_hyperparameters_test.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
-from torch._C import LockingLogger
-from wandb.proto.wandb_telemetry_pb2 import Labels
 
 
 
-
-
-f = open('C:\m_results\holy.txt','r')
+f = open('C:\m_results\wow.txt','r')
 lines = f.readlines()
 num = 0
 
 
 # ★★★★★★★★★★★★★★★밑에 9를 자동적으로 받아올수 있게끔 해야함★★★★★★★★★★★★★★★
 # 이렇게 하면 많이 성공한거
-for i in range(9):
+
+
+# ★★★★★여기도 epochs가 바뀔때마다 바꿔줘야하네★★★★★
+for i in range(20):
     locals()[f'loss_{i}'] = []
     locals()[f'lr_{i}'] = []
     locals()[f'batch_size_{i}'] = []
@@ -23,11 +22,13 @@ for line in lines:
     line = line.split()
     print(line)
     locals()[f'loss_{num}'].append(float(line[1]))
-    if line[3] == '9':
+    if line[3] == '19':
+        # ★★★★★ 여기도 epochs가 바뀔때마다 바꿔줘야하네 ★★★★★
         locals()[f'lr_{num}'].append(line[5])
         locals()[f'batch_size_{num}'].append(line[-1])
         num+=1
-        
+
+# print(loss_0)
 # print(lr_0)
 
     # split하고나면 배열형태가 되기때문에 배열을 잡아줘야 split 다시 할 수 있음
@@ -37,6 +38,7 @@ def plt_setting(amount_of_loss):
     # f'y{i}'이거 loss_0, loss_1, loss_2 이런식으로 바꿔야함 들어올때 ㅎㅎ
     for i in range(amount_of_loss):
         # lr = lr_0
+        # 여기서 들어오는 함수만 plot 할수있게 해야겠네
         lr = globals()[f'lr_{i}']
         bt_size = globals()[f'batch_size_{i}']
         plt.plot(globals()[f'loss_{i}'],label = f'lr:{float(lr[0])},b_size:{int(bt_size[0])}')
@@ -48,7 +50,7 @@ def plt_setting(amount_of_loss):
         # x축을 설정해버리면 x,y가 1대1대응이 되어야하므로 오히려 해가됨
 
     plt.xlabel('epochs')
-    plt.ylabel('loss')
+    plt.ylabel('train_loss')
 
     plt.legend()
     plt.show()
@@ -59,5 +61,8 @@ def plt_setting(amount_of_loss):
 # 다만 validation 에 최적화하는것은 무조건 가능
 
 
-plt_setting(8)
+plt_setting(12)
 f.close()
+# x=len(learning_rate)xlen(batch_size)
+# -> plot_setting(x)
+# ★★★★★이거 자동으로 만드는거 생각해보기★★★★★
